5794eb2c9b392ac17d6078509e201e7876ea6a42
Angelmaneuver <f_a-18hornet@hotmail.co.jp>
2024-04-21 11:13:53 +0000

    debug.

--- src/components/setting-dialog.tsx
+++ src/components/setting-dialog.tsx
@@ -31,17 +31,17 @@ body {
   background-repeat:   no-repeat;
 }
 
-body::before {
-  content:    '';
-  background: inherit;
-  filter:     brightness(.85);;
-  position:   absolute;
-  top:        0;
-  right:      0;
-  bottom:     0;
-  left:       0;
-  z-index:   -1;
-}
+// body::before {
+//   content:    '';
+//   background: inherit;
+//   filter:     brightness(.85);;
+//   position:   absolute;
+//   top:        0;
+//   right:      0;
+//   bottom:     0;
+//   left:       0;
+//   z-index:   -1;
+// }
 
 `;
 
